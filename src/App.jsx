3f5bddaf536eb3f94382413dd3414087c9689408
Marco-Gonzalez26 <magp2606@gmail.com>
2026-05-02 13:08:39 -0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+const App = () => {
+  return (
+    <div className='flex-center h-screen'>
+      <h1 className='text-4xl font-bold'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
